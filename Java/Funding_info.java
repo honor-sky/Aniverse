a03a19ca5d9c209012d4fe21f46d4c4d75ddbd95
kimminjeong <69973291+kim-minjeong01@users.noreply.github.com>
2021-10-28 02:28:00 +0900

    Add files via upload

---
+++ Java/Funding_info.java
@@ -0,0 +1,18 @@
+package org.tecttown.aniverse;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Funding_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_funding_info);
+    }
+}
